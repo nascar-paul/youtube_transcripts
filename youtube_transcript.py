684c669b5f094f8e59e507ca98f917729236cc3b
nascar_paul <paul.tyler.jones@gmail.com>
2023-07-15 19:53:48 +0800

    Saves the transcript to a file and comments

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -1,8 +1,37 @@
+# Import the youtube transcription module
 from youtube_transcript_api import YouTubeTranscriptApi
 
+# Defines a function to get the transcript
 def get_transcript(video_id):
+   # Use the YouTubeTranscriptApi to get the transcript of the video
+   # The transcript is returned as a list of dictionaries, where each dictionary
+   # represents a piece of the transcript
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
     return transcript
 
+# Defines a function to write the transcript to a file
+def write_transcript_to_file(video_id, transcript):
+    # Open a text file with the same name as the video_id
+    # The 'w' argument means that the file will be opened for writing
+
+        with open(f'{video_id}.txt', 'w') as file:
+        # Iterate over each entry in the transcript
+
+        for entry in transcript:
+
+		# Write the text of the entry to the file, followed by a newline character
+		# The 'text' key of each entry contains the actual text of the transcript
+
+            file.write(entry['text'] + '\n')
+
+# Set the ID of the video you want to get the transcript for
+
 video_id = 'your_video_id'
-print(get_transcript(video_id))
+
+# Get the transcript of the video
+
+transcript = get_transcript(video_id)
+
+# Write the transcript to a file
+
+write_transcript_to_file(video_id, transcript)
