c9583151bb5f84ed23e350e23f2a0150840147d1
nascar_paul <paul.tyler.jones@gmail.com>
2023-07-15 20:07:41 +0800

    Runs the transript collection

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -14,19 +14,15 @@ def write_transcript_to_file(video_id, transcript):
     # Open a text file with the same name as the video_id
     # The 'w' argument means that the file will be opened for writing
 
-        with open(f'{video_id}.txt', 'w') as file:
+    with open(f'{video_id}.txt', 'w') as file:
         # Iterate over each entry in the transcript
-
         for entry in transcript:
-
-		# Write the text of the entry to the file, followed by a newline character
-		# The 'text' key of each entry contains the actual text of the transcript
-
+            # Write the text of the entry to the file, followed by a newline character
+            # The 'text' key of each entry contains the actual text of the transcript
             file.write(entry['text'] + '\n')
 
 # Set the ID of the video you want to get the transcript for
-
-video_id = 'your_video_id'
+video_id = 'PdE-waSx-d8'
 
 # Get the transcript of the video
 
