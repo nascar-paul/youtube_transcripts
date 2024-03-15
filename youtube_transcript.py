c04067bc55504fdeeec1dcf688c875d59c1a2153
Paul Jones <paul.tyler.jones@gmail.com>
2024-03-15 14:25:01 +0000

    Generates another transcript

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -1,5 +1,6 @@
 # Import the youtube transcription module
 from youtube_transcript_api import YouTubeTranscriptApi
+import os
 
 # Defines a function to get the transcript
 def get_transcript(video_id):
@@ -31,3 +32,22 @@ transcript = get_transcript(video_id)
 # Write the transcript to a file
 
 write_transcript_to_file(video_id, transcript)
+
+def write_transcript_to_file(video_id, transcript):
+    # Get the title of the video using the YouTubeTranscriptApi
+    video_title = YouTubeTranscriptApi.get_transcript(video_id)[0]['title']
+    
+    # Remove any invalid characters from the video title
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    video_title = ''.join(c for c in video_title if c in valid_chars)
+    
+    # Rename the file to the video title
+    new_filename = f'{video_title}.txt'
+    os.rename(f'{video_id}.txt', new_filename)
+    
+    # Open the renamed file for writing
+    with open(new_filename, 'w') as file:
+        # Iterate over each entry in the transcript
+        for entry in transcript:
+            # Write the text of the entry to the file, followed by a newline character
+            file.write(entry['text'] + '\n')
